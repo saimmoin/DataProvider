d3700aba8b30d82172778e47d8e82e89f216fab8
Saim Saqib <73789776+saimmoin@users.noreply.github.com>
2023-09-07 09:58:46 +0500

    refresh token completed

--- DataProvider/src/main/java/com/DataProvider/DataProvider/Repository/RefreshTokenRepo.java
+++ DataProvider/src/main/java/com/DataProvider/DataProvider/Repository/RefreshTokenRepo.java
@@ -1,10 +1,11 @@
 package com.DataProvider.DataProvider.Repository;
 
+import com.DataProvider.DataProvider.Entity.Employee;
 import com.DataProvider.DataProvider.Entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepo extends JpaRepository <RefreshToken,Integer>{
 
     RefreshToken findByToken(String token);
-
+    RefreshToken findByUser(Employee e);
 }
